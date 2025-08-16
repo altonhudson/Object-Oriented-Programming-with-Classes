7f5122d5ed3ea8de55dc691858f69dd6b6c35100
altonhudson <ah_1014@hotmail.com>
2025-08-15 23:37:47 -0400

    Setting up Classes

---
+++ script.js
@@ -0,0 +1,21 @@
+
+class productProperties {
+    constructor(name, price, quantity) {
+        this.name = name
+        this.price = price
+        this.quantity = quantity
+    }
+
+    getTotalValue() {
+        return this.price * this.quantity
+    }
+    
+    toString() {
+        return `Product: ${this.name}, Price: $${this.price}, Quantity: ${this.quantity}`;
+    }
+}
+
+let product = new productProperties("Apple", 2.50, 4)
+
+console.log(product.getTotalValue());
+console.log(product.toString());
