1d7db20fdd6dfe91c302861a84608a091dfb10a0
altonhudson <ah_1014@hotmail.com>
2025-08-16 00:05:27 -0400

    Adding Inheritance update

--- script.js
+++ script.js
@@ -21,8 +21,16 @@ let product = new productProperties("Apple", 2.50, 4)
 // console.log(product.toString());
 
 class perishableProductProperties extends productProperties {
-    constructor(expire) {
+    constructor(name, price, quantity, expire) {
         super(name, price, quantity)
-        this.expirationDate = expirationDate
+        this.expire = expire
+    }
+
+    expirationDate() {
+         return `Product: ${this.name}, Price: $${this.price}, Quantity: ${this.quantity}, Expiration Date: ${this.expire}`
     }
 }
+
+const perishable = new perishableProductProperties("Orange", 1.99, 6, "2025-11-30")
+
+console.log(perishable.expirationDate());
